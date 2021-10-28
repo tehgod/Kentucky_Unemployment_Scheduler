b515be3adfc6ab46c7a01b070f7bde1647427237
Shelby <shelbygt500martin@gmail.com>
2021-10-28 18:29:49 -0400

    First version, with all personal information and passwords removed

---
+++ KY_unemployment_scheduler.py
@@ -0,0 +1,76 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import json
+from datetime import datetime
+import win32com.client as win32
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+sender_email_address = os.getenv('SENDER_EMAIL_ADDRESS')
+sender_email_password = os.getenv('SENDER_EMAIL_PASSWORD')
+recipient_email_address = os.getenv('RECIPIENT_EMAIL_ADDRESS')
+desired_location = os.getenv('DESIRED_LOCATION')
+
+def check_availability():
+    my_response =requests.post("https://telegov.egov.com/lc_ui/CustomerCreateAppointments/SelectType")
+    my_response =requests.post("https://telegov.egov.com/lc_ui/AppointmentWizard/61")
+    my_response = my_response.text
+    soup = BeautifulSoup(my_response, 'lxml')
+    try:
+        return soup.find('div', class_= "badge badge-pill badge-danger text-wrap mb-2").text
+    except:
+        return ("Availability")
+
+def list_openings():
+    my_response =requests.post("https://telegov.egov.com/lc_ui/AppointmentWizard/61")
+    my_response = my_response.text
+    soup = BeautifulSoup(my_response, 'lxml')
+    data = soup.find_all("script", type="text/javascript")[3].get_text()
+    my_json = (data[data.find("["):data.find("]")+1])
+    my_json_var = json.loads(my_json)
+    for item in my_json_var:
+        if item['IsFullyBooked'] == False:
+            now = datetime.now()
+            print (f"{now}: {item['City']} has an opening")
+            if item['City']==desired_location:
+                send_email_notification(recipient_email_address)
+                return True
+            
+
+def send_email_notification(outbound_email):
+    now = datetime.now()
+    msg = EmailMessage()
+    msg['From'] = sender_email_address
+    msg['To'] = outbound_email
+    msg['Subject'] = 'Appointment Opening'
+    msg.set_content(f"Current appointment availablility now at Louisville for the unemployment office.\nCurrent time of opening is: {now}\n Sign up now at the following link: https://telegov.egov.com/lc_ui/AppointmentWizard/61")
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(sender_email_address, sender_email_password)
+        server.send_message(msg)
+        server.quit()
+        print ('Email Sent!')
+        sleep(300)
+    except:
+        print ('Something went wrong when attempting to send email')
+
+def run_script(my_bool):
+    while my_bool == True:
+        now = datetime.now()
+        if check_availability()==('No Availability'):
+            print ('--------------------')
+            print (f"{now}: no appointments at this time")
+            sleep(6)
+        else:
+            print ('--------------------')
+            print (f'{now}: Openings found at below locations: ')
+            if list_openings() == True:
+                my_bool = False
+            sleep (30)
+            
+run_script(True)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Kentucky Unemployment Notification
+
+This tool is used for scheduling an in-person appointment for the Kentucky unemployment office.
+
+## Installation
+
+Use the included requirements.txt to install needed packages, this will require Python to run.
+
+'''bash
+pip install requirements.txt
+'''
+
+You will also need to have a .env file or your environment variables set with your sending email address, sending email password, recipient email , and desired location for appointment.
+By default, it is looking for these variables under the following names: SENDER_EMAIL_ADDRESS , SENDER_EMAIL_PASSWORD , RECIPIENT_EMAIL_ADDRESS, and DESIRED_LOCATION .
+
+## Contributing
+
+All pull requests are more than welcome, I can't imagine I will be updating this script too much but will be very glad to update upon request, or recommendation. 
+
+If you encounter any errors, please just report in Github 'Issues' category, and it will be fixed promptly.
+
+

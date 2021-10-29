66bbd4c8c8e864fc71d8b04f91dbd6bf372a458f
Shelby <shelbygt500martin@gmail.com>
2021-10-28 22:25:18 -0400

    adding options to create env file via command line

--- KY_unemployment_scheduler.py
+++ KY_unemployment_scheduler.py
@@ -9,12 +9,40 @@ from email.message import EmailMessage
 from dotenv import load_dotenv
 import os
 
+sender_email_address = ""
+sender_email_password = ""
+recipient_email_address = ""
+desired_location = ""
 
-load_dotenv()
-sender_email_address = os.getenv('SENDER_EMAIL_ADDRESS')
-sender_email_password = os.getenv('SENDER_EMAIL_PASSWORD')
-recipient_email_address = os.getenv('RECIPIENT_EMAIL_ADDRESS')
-desired_location = os.getenv('DESIRED_LOCATION')
+
+
+
+def configure_stored_credentials():
+    user_input = input("Would you like to restore previous settings, or setup new credentials?\n1.Use stored credentials.\n2.Setup new credentials.\n3.Exit\n")
+    while user_input not in list(range(1,4)):
+        user_input = input("Please try again. Please only put in the number of your response, and press enter.\n")
+    if user_input == 3:
+        return False
+    elif user_input == 1:
+        global sender_email_address, sender_email_password, recipient_email_address, desired_location
+        load_dotenv()
+        sender_email_address = os.getenv('SENDER_EMAIL_ADDRESS')
+        sender_email_password = os.getenv('SENDER_EMAIL_PASSWORD')
+        recipient_email_address = os.getenv('RECIPIENT_EMAIL_ADDRESS')
+        desired_location = os.getenv('DESIRED_LOCATION')
+        return True
+    elif user_input == 2:
+
+        return True
+    else:
+        print ('Unexpected error, closing out application.')
+        return False
+
+    # if user_input not in range(3):
+    # if user_input == 1:
+    # elif user_input ==2:
+    # elif user_input ==3:
+    # else:
 
 def check_availability():
     my_response =requests.post("https://telegov.egov.com/lc_ui/CustomerCreateAppointments/SelectType")
@@ -61,6 +89,7 @@ def send_email_notification(outbound_email):
 
 def run_script(my_bool):
     while my_bool == True:
+        configure_stored_credentials()
         now = datetime.now()
         if check_availability()==('No Availability'):
             print ('--------------------')
@@ -73,4 +102,5 @@ def run_script(my_bool):
                 my_bool = False
             sleep (30)
             
-run_script(True)
+#run_script(True)
+configure_stored_credentials()
